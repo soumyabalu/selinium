599ce23625c129aca4f13d2bbeaab71331309af1
soumya balu <soumya balu@192.168.1.17>
2020-01-05 07:23:13 -0500

    Class name changed

--- Introduction/src/Introduction/Facebooktest.java
+++ Introduction/src/Introduction/Facebooktest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class Firstpro {
+public class Facebooktest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
