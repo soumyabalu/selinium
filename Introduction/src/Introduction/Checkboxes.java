a2b96b83842abcb97034f59ab81143912ff8cc83
soumya balu <soumya balu@192.168.1.17>
2020-01-12 15:47:25 -0500

    test case for checkboxes

---
+++ Introduction/src/Introduction/Checkboxes.java
@@ -0,0 +1,22 @@
+package Introduction;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Checkboxes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\soumya balu\\workspace\\New folder\\chromedriver_win32\\chromedriver.exe"); 
+		WebDriver myobj = new ChromeDriver();
+		myobj.get("https://www.spicejet.com/");
+		System.out.println(myobj.findElement(By.cssSelector("input[id='ctl00_mainContent_chk_friendsandfamily']")).isSelected());
+		myobj.findElement(By.cssSelector("input[id*='ctl00_mainContent_chk_friendsandfamily']")).click();
+		System.out.println(myobj.findElement(By.cssSelector("input[id='ctl00_mainContent_chk_friendsandfamily']")).isSelected());
+        //count of the checkboxes to br find out.
+		//used findElements instead of findElement to find out all the checkbox numbers
+		System.out.println(myobj.findElements(By.cssSelector("input[type='checkbox']")).size());
+	}
+
+}
